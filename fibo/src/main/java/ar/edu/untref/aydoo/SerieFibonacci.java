7f3d5eda85f55517c125a4ddecb4a828fe864b5f
WaldoGaspari <waldogaspari@gmail.com>
2018-03-25 20:07:21 -0300

    Se agregan mas tests

--- fibo/src/main/java/ar/edu/untref/aydoo/SerieFibonacci.java
+++ fibo/src/main/java/ar/edu/untref/aydoo/SerieFibonacci.java
@@ -4,11 +4,9 @@ public class SerieFibonacci {
 
 	public static int calcularSerieDeFibonacci(int numero) {	
 		if(numero == 0){
-			return 0;
-			
+			return 0;	
 		} else if (numero == 1){
 			return 1;
-			
 		} else {
 			return (calcularSerieDeFibonacci(numero-1) + calcularSerieDeFibonacci(numero-2));
 		}
--- fibo/src/test/java/ar/edu/untref/aydoo/FiboTest.java
+++ fibo/src/test/java/ar/edu/untref/aydoo/FiboTest.java
@@ -8,48 +8,56 @@ public class FiboTest {
 	@Test
 	public void calcularSerieFibonacci2(){
 		
-		SerieFibonacci serie = new SerieFibonacci();
-		int resultado = serie.calcularSerieDeFibonacci(2);
+		int resultado = SerieFibonacci.calcularSerieDeFibonacci(2);
 		Assert.assertEquals(1, resultado);
 	}
 	
 	@Test
 	public void calcularSerieFibonacci3(){
 		
-		SerieFibonacci serie = new SerieFibonacci();
-		int resultado = serie.calcularSerieDeFibonacci(3);
+		int resultado = SerieFibonacci.calcularSerieDeFibonacci(3);
 		Assert.assertEquals(2, resultado);
 	}
 	
 	@Test
 	public void calcularSerieFibonacci5(){
 		
-		SerieFibonacci serie = new SerieFibonacci();
-		int resultado = serie.calcularSerieDeFibonacci(5);
+		int resultado = SerieFibonacci.calcularSerieDeFibonacci(5);
 		Assert.assertEquals(5, resultado);
 	}
 	
 	@Test
 	public void calcularSerieFibonacci8(){
 		
-		SerieFibonacci serie = new SerieFibonacci();
-		int resultado = serie.calcularSerieDeFibonacci(8);
+		int resultado = SerieFibonacci.calcularSerieDeFibonacci(8);
 		Assert.assertEquals(21, resultado);
 	}
 	
 	@Test
 	public void calcularSerieFibonacci0(){
 		
-		SerieFibonacci serie = new SerieFibonacci();
-		int resultado = serie.calcularSerieDeFibonacci(0);
+		int resultado = SerieFibonacci.calcularSerieDeFibonacci(0);
 		Assert.assertEquals(0, resultado);
 	}
 	
 	@Test
 	public void calcularSerieFibonacci1(){
 		
-		SerieFibonacci serie = new SerieFibonacci();
-		int resultado = serie.calcularSerieDeFibonacci(1);
+		int resultado = SerieFibonacci.calcularSerieDeFibonacci(1);
 		Assert.assertEquals(1, resultado);
+	}
+	
+	@Test
+	public void calcularSerieFibonacci12(){
+		
+		int resultado = SerieFibonacci.calcularSerieDeFibonacci(12);
+		Assert.assertEquals(144, resultado);
+	}
+	
+	@Test
+	public void calcularSerieFibonacci17(){
+		
+		int resultado = SerieFibonacci.calcularSerieDeFibonacci(17);
+		Assert.assertEquals(1597, resultado);
 	}
 }
