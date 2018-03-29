e1fb32e3ecc04c1af7569fccc24331a1233058e1
WaldoGaspari <waldogaspari@gmail.com>
2018-03-29 19:41:49 -0300

    Cambio en la entrada del Program

--- fibo/src/main/java/ar/edu/untref/aydoo/Program.java
+++ fibo/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -7,7 +7,6 @@ public class Program {
     public static final void main(final String[] arg) {
     	
     	Scanner entrada = new Scanner(System.in);
-	    System.out.println("Escribe un numero");
 	    int numero = entrada.nextInt();
 	    int[] resultado = new int[numero];
 	    for(int i = 0; i<numero; i++){
