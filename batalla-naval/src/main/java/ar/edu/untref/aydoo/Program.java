656d3f39bb312abad9b8cd62853f6953b6c8cf64
WaldoGaspari <waldogaspari@gmail.com>
2018-04-07 14:41:38 -0300

    Se agregan las clases del ejercicio realizadas en clase y en grupo

--- batalla-naval/src/main/java/ar/edu/untref/aydoo/Program.java
+++ batalla-naval/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -1,11 +1,8 @@
 package ar.edu.untref.aydoo;
 
 public class Program {
+	
+	public static final void main(String arg[]) throws Exception {
 
-    protected Program() {
-    }
-
-    public static final void main(final String[] arg) {
-        System.out.println("hola!");
-    }
+	}
 }
