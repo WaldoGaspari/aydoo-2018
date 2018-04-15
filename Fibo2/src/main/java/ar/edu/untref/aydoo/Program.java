75b7a56020065e9c0f4370839a52475da2e41fb8
WaldoGaspari <waldogaspari@gmail.com>
2018-04-15 19:49:11 -0300

    Al ingresar algun parametro del formato incorrecto tira el mensaje correspondiente

--- Fibo2/src/main/java/ar/edu/untref/aydoo/Program.java
+++ Fibo2/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -2,21 +2,26 @@ package ar.edu.untref.aydoo;
 
 public class Program {
 
-	static int numero = 0;
-	static int[] resultado;
+	private static int numero;
+	private static int[] resultado;
 	
     public static final void main(final String[] arg) {
     	if (arg.length == 2) {
     		String formato = String.valueOf(arg[0]);
     	    char orientacion = formato.charAt(3);
     	    char direccion = formato.charAt(4);
-    	    numero = Integer.parseInt(arg[1]);
-    	    resultado = new int[numero];
-    	    calcularSerie();
-    	    if (direccion == 'i') {
-    	    	resultado = ordenarEnFormaDescendente(resultado);
+    	    if ((orientacion != 'h' && orientacion != 'v') || (direccion != 'd' && direccion != 'i')) {
+    	    	System.out.printf("Opcion no valida");
+    	    	
+    	    } else {
+    	    	numero = Integer.parseInt(arg[1]);
+        	    resultado = new int[numero];
+        	    calcularSerie();
+        	    if (direccion == 'i') {
+        	    	resultado = ordenarEnFormaDescendente(resultado);
+        	    }
+        	    mostrarNumerosSegunOrientacion(orientacion);
     	    }
-    	    mostrarNumerosSegunOrientacion(orientacion);
     	    
     	} else {
     		numero = Integer.parseInt(arg[0]);
@@ -26,11 +31,11 @@ public class Program {
     	}
     }
     
-    private static int[] ordenarEnFormaDescendente(int[] numeros){
+    private static int[] ordenarEnFormaDescendente(int[] numeros) {
 		int[] numerosEnOrdenDescendente = numeros;
-		for (int i = 0; i < numerosEnOrdenDescendente.length-1; i++){
-			for (int j = i+1; j < numerosEnOrdenDescendente.length; j++){
-				if(numerosEnOrdenDescendente[i] < numerosEnOrdenDescendente[j]){
+		for (int i = 0; i < numerosEnOrdenDescendente.length - 1; i++) {
+			for (int j = i + 1; j < numerosEnOrdenDescendente.length; j++) {
+				if (numerosEnOrdenDescendente[i] < numerosEnOrdenDescendente[j]) {
 					int auxiliar = numerosEnOrdenDescendente[i];
 					numerosEnOrdenDescendente[i] = numerosEnOrdenDescendente[j];
 					numerosEnOrdenDescendente[j] = auxiliar;	
