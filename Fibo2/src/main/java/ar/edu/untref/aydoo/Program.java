d935b0e45350a3eee00b1e2900e24d9843cb76fa
WaldoGaspari <waldogaspari@gmail.com>
2018-04-15 16:57:43 -0300

    Refactor en la clase Program. Se puede ver el resultado en cualquier orientacion y direccion y ademas se puede ver el resultado de cualquier numero por default

--- Fibo2/src/main/java/ar/edu/untref/aydoo/Program.java
+++ Fibo2/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -2,22 +2,46 @@ package ar.edu.untref.aydoo;
 
 public class Program {
 
+	static int numero = 0;
+	static int[] resultado;
+	
     public static final void main(final String[] arg) {
-	    String formato = String.valueOf(arg[0]);
-	    char orientacion = formato.charAt(3);
-	    char direccion = formato.charAt(4);
-    	int numero = Integer.parseInt(arg[1]);
-	    int[] resultado = new int[numero];
-	    for (int i = 0; i < numero; i++) {
-	    	resultado[i] = SerieFibonacci.calcularSerieDeFibonacci(i);
-	    }
-	    
-	    if (direccion == 'i') {
-	    	resultado = ordenarEnFormaDescendente(resultado);
-	    }
-	    
-	    System.out.printf("fibo<%d>: ", numero);
-	    
+    	if (arg.length == 2) {
+    		String formato = String.valueOf(arg[0]);
+    	    char orientacion = formato.charAt(3);
+    	    char direccion = formato.charAt(4);
+    	    numero = Integer.parseInt(arg[1]);
+    	    resultado = new int[numero];
+    	    calcularSerie();
+    	    if (direccion == 'i') {
+    	    	resultado = ordenarEnFormaDescendente(resultado);
+    	    }
+    	    mostrarNumerosSegunOrientacion(orientacion);
+    	    
+    	} else {
+    		numero = Integer.parseInt(arg[0]);
+    	    resultado = new int[numero];
+    	    calcularSerie();
+    	    mostrarNumerosSegunOrientacion('h');
+    	}
+    }
+    
+    private static int[] ordenarEnFormaDescendente(int[] numeros){
+		int[] numerosEnOrdenDescendente = numeros;
+		for (int i = 0; i < numerosEnOrdenDescendente.length-1; i++){
+			for (int j = i+1; j < numerosEnOrdenDescendente.length; j++){
+				if(numerosEnOrdenDescendente[i] < numerosEnOrdenDescendente[j]){
+					int auxiliar = numerosEnOrdenDescendente[i];
+					numerosEnOrdenDescendente[i] = numerosEnOrdenDescendente[j];
+					numerosEnOrdenDescendente[j] = auxiliar;	
+				}
+			}
+		}
+		return numerosEnOrdenDescendente;
+	}
+    
+    private static void mostrarNumerosSegunOrientacion(char orientacion) {
+    	System.out.printf("fibo<%d>: ", numero);
 	    if (orientacion == 'v') {
 	    	System.out.printf("\r\n");
 	    	for (int i = 0; i < numero; i++) {	
@@ -31,17 +55,9 @@ public class Program {
 	    }
     }
     
-    private static int[] ordenarEnFormaDescendente(int[] numeros){
-		int[] numerosEnOrdenDescendente = numeros;
-		for (int i = 0; i < numerosEnOrdenDescendente.length-1; i ++){
-			for (int j = i+1; j < numerosEnOrdenDescendente.length; j ++){
-				if(numerosEnOrdenDescendente[i] < numerosEnOrdenDescendente[j]){
-					int auxiliar = numerosEnOrdenDescendente[i];
-					numerosEnOrdenDescendente[i] = numerosEnOrdenDescendente[j];
-					numerosEnOrdenDescendente[j] = auxiliar;	
-				}
-			}
-		}
-		return numerosEnOrdenDescendente;
-	}
+    private static void calcularSerie() {
+    	for (int numeroDeLaSerie = 0; numeroDeLaSerie < numero; numeroDeLaSerie++) {
+	    	resultado[numeroDeLaSerie] = SerieFibonacci.calcularSerieDeFibonacci(numeroDeLaSerie);
+	    }
+    }
 }
