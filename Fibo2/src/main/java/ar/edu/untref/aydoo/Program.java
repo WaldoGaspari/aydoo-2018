8f8280b1c5be68b22cfe6784da3afd1dc303a447
WaldoGaspari <waldogaspari@gmail.com>
2018-04-15 15:36:38 -0300

    Se puede ver la serie de Fibonacci de cualquier numero en forma horizontal y en direccion directa o inversa

--- Fibo2/src/main/java/ar/edu/untref/aydoo/Program.java
+++ Fibo2/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -3,14 +3,36 @@ package ar.edu.untref.aydoo;
 public class Program {
 
     public static final void main(final String[] arg) {
-	    int numero = Integer.parseInt(arg[0]);
+	    String formato = String.valueOf(arg[0]);
+	    //char orientacion = formato.charAt(3);
+	    char direccion = formato.charAt(4);
+    	int numero = Integer.parseInt(arg[1]);
 	    int[] resultado = new int[numero];
 	    for (int i = 0; i < numero; i++) {
 	    	resultado[i] = SerieFibonacci.calcularSerieDeFibonacci(i);
 	    }
+	    
+	    if (direccion == 'i') {
+	    	resultado = ordenarEnFormaDescendente(resultado);
+	    }
+	    
 	    System.out.printf("fibo<%d>: ", numero);
 	    for (int i = 0; i < numero; i++) {	
 	    	System.out.printf("%d ", resultado[i]);
 	    }
     }
+    
+    private static int[] ordenarEnFormaDescendente(int[] numeros){
+		int[] numerosEnOrdenDescendente = numeros;
+		for (int i = 0; i < numerosEnOrdenDescendente.length-1; i ++){
+			for (int j = i+1; j < numerosEnOrdenDescendente.length; j ++){
+				if(numerosEnOrdenDescendente[i] < numerosEnOrdenDescendente[j]){
+					int auxiliar = numerosEnOrdenDescendente[i];
+					numerosEnOrdenDescendente[i] = numerosEnOrdenDescendente[j];
+					numerosEnOrdenDescendente[j] = auxiliar;	
+				}
+			}
+		}
+		return numerosEnOrdenDescendente;
+	}
 }
