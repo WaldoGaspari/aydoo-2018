73ab9948c7d7710773090f4150908512fd56cee1
WaldoGaspari <waldogaspari@gmail.com>
2018-04-16 19:10:06 -0300

    Se agrega la carpeta para comenzar ejercicio de libreria

---
+++ ejercicio_libreria/src/main/java/ar/edu/untref/aydoo/SerieFibonacci.java
@@ -0,0 +1,14 @@
+package ar.edu.untref.aydoo;
+
+public class SerieFibonacci {
+
+	public static int calcularSerieDeFibonacci(int numero) {	
+		if (numero == 0) {
+			return 0;	
+		} else if (numero == 1) {
+			return 1;
+		} else {
+			return (calcularSerieDeFibonacci(numero - 1) + calcularSerieDeFibonacci(numero - 2));
+		}
+	}
+}
