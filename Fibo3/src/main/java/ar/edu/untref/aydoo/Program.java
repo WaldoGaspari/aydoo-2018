275f97209e846fe956930804f2e1df6bf5fb82c1
WaldoGaspari <waldogaspari@gmail.com>
2018-04-28 14:35:37 -0300

    Se puede generar el archivo de salida y copia el resultado del programa en el mismo. Solo lo hace como en fibo2.

--- Fibo3/src/main/java/ar/edu/untref/aydoo/Program.java
+++ Fibo3/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -1,26 +1,33 @@
 package ar.edu.untref.aydoo;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 public class Program {
 
 	private static int numero;
 	private static int[] resultado;
 	
     public static final void main(final String[] arg) {
-    	if (arg.length == 2) {
+    	if (arg.length != 0) {
     		String formato = String.valueOf(arg[0]);
+    		String salida = String.valueOf(arg[1]);
     	    char orientacion = formato.charAt(3);
     	    char direccion = formato.charAt(4);
     	    if ((orientacion != 'h' && orientacion != 'v') || (direccion != 'd' && direccion != 'i')) {
     	    	System.out.printf("Opciones no validas");
     	    	
     	    } else {
-    	    	numero = Integer.parseInt(arg[1]);
+    	    	numero = Integer.parseInt(arg[4]);
         	    resultado = new int[numero];
         	    calcularSerie();
         	    if (direccion == 'i') {
         	    	resultado = ordenarEnFormaDescendente(resultado);
         	    }
         	    mostrarNumerosSegunOrientacion(orientacion);
+        	    escribirSobreArchivo(salida, orientacion);
     	    }
     	    
     	} else {
@@ -64,5 +71,36 @@ public class Program {
     	for (int numeroDeLaSerie = 0; numeroDeLaSerie < numero; numeroDeLaSerie++) {
 	    	resultado[numeroDeLaSerie] = SerieFibonacci.calcularSerieDeFibonacci(numeroDeLaSerie);
 	    }
+    }
+    
+    private static void escribirSobreArchivo(String salida, char orientacion) {
+    	File archivo;
+    	BufferedWriter escritura;
+    	PrintWriter pw;
+    	
+    	try {
+    		archivo = new File(salida);
+			escritura = new BufferedWriter(new FileWriter(archivo));
+			pw = new PrintWriter(escritura);
+			pw.write("fibo< >: ");
+			if (orientacion == 'v') {
+				escritura.newLine();
+				for(int i = 0; i < resultado.length; i++){
+					String valor = String.valueOf(resultado[i]);
+					escritura.write(valor);
+					escritura.newLine();
+				}
+				
+			} else {
+				for(int i = 0; i < resultado.length; i++){
+					String valor = String.valueOf(resultado[i]);
+					escritura.write(valor + " ");
+				}
+			}
+			pw.close();
+			escritura.close();
+    	}catch (Exception e) {
+    		
+    	}
     }
 }
