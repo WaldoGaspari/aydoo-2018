edff5766d47556818b53baac8f95ad9982fde0f1
WaldoGaspari <waldogaspari@gmail.com>
2018-04-30 20:40:41 -0300

    Se agregan algunos test.

--- Fibo3/src/test/java/ar/edu/untref/aydoo/ProgramTest.java
+++ Fibo3/src/test/java/ar/edu/untref/aydoo/ProgramTest.java
@@ -71,4 +71,44 @@ public class ProgramTest {
 	    Assert.assertEquals(esperado, salida);
 	}
 	
+	@Test
+	public void probarSalidaDelMainEnFormaVerticalYFuncionamientoSumatoriaDeOchoDa33() {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	    System.setOut(new PrintStream(baos));
+	    String esperado = "fibo<8>s: \r\n"
+	    		+ "33 ";
+	    String[] argumentos = {"-o=vi", "-m=s", "8"};
+
+	    Program.main(argumentos);
+	    String salida = new String(baos.toByteArray());
+
+	    Assert.assertEquals(esperado, salida);
+	}
+	
+	@Test
+	public void probarSalidaDelMainEnFormaHorizontalYFuncionamientoSumatoriaDeOchoDa33() {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	    System.setOut(new PrintStream(baos));
+	    String esperado = "fibo<8>s: 33";
+	    String[] argumentos = {"-o=hi", "-m=s", "8"};
+
+	    Program.main(argumentos);
+	    String salida = new String(baos.toByteArray());
+
+	    Assert.assertEquals(esperado, salida);
+	}
+	
+	@Test
+	public void probarSalidaDelMainEnFormaHorizontalYFuncionamientoListaDeOcho() {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	    System.setOut(new PrintStream(baos));
+	    String esperado = "fibo<8>: 13 8 5 3 2 1 1 0 ";
+	    String[] argumentos = {"-o=hi", "-m=l", "8"};
+
+	    Program.main(argumentos);
+	    String salida = new String(baos.toByteArray());
+
+	    Assert.assertEquals(esperado, salida);
+	}
+	
 }
