70af97915bac3ddaf1065fd48c1762835775b331
WaldoGaspari <waldogaspari@gmail.com>
2018-05-01 20:45:04 -0300

    Se verifica que se obtengan varios votos para un candidato y que no se cuenten si se trata de otro candidato.

--- ejercicio_votaciones/src/test/java/ar/edu/untref/aydoo/JuntaElectoralTest.java
+++ ejercicio_votaciones/src/test/java/ar/edu/untref/aydoo/JuntaElectoralTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class JuntaElectoralTest {
 	
 	@Test
-	public void sgsgsdgg() {
+	public void calcularCantidadDeVotosParaUnCandidatoQueFueVotadoSoloUnaVezDeberiaDarUno() {
 		JuntaElectoral juntaDeLasPaso = new JuntaElectoral();
 		Elector pedro = new Elector();
 		Candidato martinPaz = new Candidato();
@@ -17,6 +17,46 @@ public class JuntaElectoralTest {
 		juntaDeLasPaso.guardarVoto(voto);
 		
 		Assert.assertEquals(1, juntaDeLasPaso.contarVotosDeUnCandidato(martinPaz));
+	}
+	
+	@Test
+	public void calcularCantidadDeVotosParaUnCandidatoQueFueVotadoTresVecesDeberiaDarTres() {
+		JuntaElectoral juntaDeLasPaso = new JuntaElectoral();
+		Elector pedro = new Elector();
+		Elector juan = new Elector();
+		Elector manuel = new Elector();
+		Candidato martinPaz = new Candidato();
+		
+		Voto unVoto = pedro.votar(martinPaz);
+		Voto segundoVoto = juan.votar(martinPaz);
+		Voto tercerVoto = manuel.votar(martinPaz);
+		juntaDeLasPaso.guardarVoto(unVoto);
+		juntaDeLasPaso.guardarVoto(segundoVoto);
+		juntaDeLasPaso.guardarVoto(tercerVoto);
+		
+		Assert.assertEquals(3, juntaDeLasPaso.contarVotosDeUnCandidato(martinPaz));
+	}
+	
+	@Test
+	public void calcularCantidadDeVotosParaUnCandidatoQueFueVotadoDosVecesHabiendoOtroCandidatoDeberiaDarDos() {
+		JuntaElectoral juntaDeLasPaso = new JuntaElectoral();
+		Elector maria = new Elector();
+		Elector juana = new Elector();
+		Elector lucas = new Elector();
+		Elector juan = new Elector();
+		Candidato martinPaz = new Candidato();
+		Candidato pedroMartinez = new Candidato();
+		
+		Voto unVoto = maria.votar(martinPaz);
+		Voto segundoVoto = juana.votar(pedroMartinez);
+		Voto tercerVoto = lucas.votar(martinPaz);
+		Voto cuartoVoto = juan.votar(pedroMartinez);
+		juntaDeLasPaso.guardarVoto(unVoto);
+		juntaDeLasPaso.guardarVoto(segundoVoto);
+		juntaDeLasPaso.guardarVoto(tercerVoto);
+		juntaDeLasPaso.guardarVoto(cuartoVoto);
+		
+		Assert.assertEquals(2, juntaDeLasPaso.contarVotosDeUnCandidato(martinPaz));
 	}
 
 }
