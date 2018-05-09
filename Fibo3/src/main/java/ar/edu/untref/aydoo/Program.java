d3ba6b309a78926a35f47614718fa6af4ef5fa15
WaldoGaspari <waldogaspari@gmail.com>
2018-05-08 23:57:07 -0300

    Cambio en el diseño del programa. Se agregan nuevas clases para sacar responsabilidades a la clase Program.

--- Fibo3/src/main/java/ar/edu/untref/aydoo/Program.java
+++ Fibo3/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -1,9 +1,5 @@
 package ar.edu.untref.aydoo;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-
 public class Program {
 
 	private static int numero;
@@ -15,10 +11,10 @@ public class Program {
     public static final void main(final String[] arg) {
 	    numero = Integer.parseInt(arg[arg.length - 1]);
 	    resultado = new int[numero];
-	    calcularSerie();
+	    SerieFibonacci.calcularSerie(numero, resultado);
 	    
 	    if (arg.length == 1) {
-	    	mostrarNumerosSegunOrientacion('h');
+	    	ImpresionPantalla.mostrarNumerosSegunOrientacion('h', numero, resultado);
 	    	
 	    } else {
 	    	String modoFuncionamiento = null;
@@ -26,7 +22,7 @@ public class Program {
 	    	String salida = null;
 	    	String[] salidaPartes = null;
 	    	String nombreDelArchivo = null;
-	    	for (int i = 0; i < arg.length-1; i++) {
+	    	for (int i = 0; i < arg.length - 1; i++) {
 	    		if (arg[i].startsWith("-o")) {
 	    			formato = String.valueOf(arg[i]);
 	    			orientacion = formato.charAt(3);
@@ -54,13 +50,13 @@ public class Program {
 		    		if (direccion == 'i') {
 				    	resultado = ordenarEnFormaDescendente(resultado);
 				    }
-				    mostrarNumerosSegunOrientacion(orientacion);
+		    		ImpresionPantalla.mostrarNumerosSegunOrientacion(orientacion, numero, resultado);
 				    
 		    	} else if (formato != null && modoFuncionamiento != null && salida == null) {
-		    		mostrarNumerosSegunModoDeFuncionamiento(funcionamiento, orientacion);
+		    		ImpresionPantalla.mostrarNumerosSegunModoDeFuncionamiento(funcionamiento, orientacion, numero, resultado);
 		    		
 		    	} else {
-		    		escribirSobreArchivo(nombreDelArchivo, funcionamiento);
+		    		Archivo.escribirSobreArchivo(nombreDelArchivo, funcionamiento, orientacion, numero, resultado);
 		    	}
 		    }
 	    }
@@ -78,93 +74,5 @@ public class Program {
 			}
 		}
 		return numerosDescendentes;
-	}
-    
-    private static void mostrarNumerosSegunOrientacion(char orientacion) {
-    	System.out.printf("fibo<%d>: ", numero);
-	    if (orientacion == 'v') {
-	    	System.out.printf("\r\n");
-	    	for (int i = 0; i < numero; i++) {
-		    	System.out.printf("%d \r\n", resultado[i]);
-		    }
-	    
-	    } else {
-	    	for (int i = 0; i < numero; i++) {
-		    	System.out.printf("%d ", resultado[i]);
-		    }
-	    }
-    }
-    
-    private static void mostrarNumerosSegunModoDeFuncionamiento(final char funcionamiento, char orientacion) {
-    	if (funcionamiento == 'l') {
-			mostrarNumerosSegunOrientacion(orientacion);
-			
-		} else {
-			if (orientacion == 'h') {
-				System.out.printf("fibo<%d>s: %d", numero, sumatoriaDeLaSerieDeFibonacci(resultado));
-				
-			} else {
-				System.out.printf("fibo<%d>s: ", numero);
-				System.out.printf("\r\n");
-				System.out.printf("%d ", sumatoriaDeLaSerieDeFibonacci(resultado));
-			}
-		}
-    }
-    
-    private static void calcularSerie() {
-    	for (int numeroDeLaSerie = 0; numeroDeLaSerie < numero; numeroDeLaSerie++) {
-	    	resultado[numeroDeLaSerie] = SerieFibonacci.calcularElementoEnLaSerieDeFibonacci(numeroDeLaSerie);
-	    }
-    }
-    
-    private static void escribirSobreArchivo(String archivoDestino, char funcionamiento) {
-		if (funcionamiento == ' ') {
-			funcionamiento = 'l';
-		}
-    	File archivo;
-    	BufferedWriter escritura;
-    	System.out.printf("fibo<%d> guardado en %s.txt", numero, archivoDestino);
-    	try {
-        	archivo = new File(archivoDestino);
-    		escritura = new BufferedWriter(new FileWriter(archivo));
-    		if (funcionamiento == 'l') {
-    			escritura.write("fibo<" + String.valueOf(numero) + ">: ");
-        		if (orientacion == 'v') {
-        			escritura.newLine();
-        			for (int posicion = 0; posicion < resultado.length; posicion++) {
-        				String valor = String.valueOf(resultado[posicion]);
-        				escritura.write(valor);
-        				escritura.newLine();
-        			}
-        				
-        		} else {
-        			for (int posicion = 0; posicion < resultado.length; posicion++) {
-        				String valor = String.valueOf(resultado[posicion]);
-        				escritura.write(valor + " ");
-        			}
-        		}
-        		
-    		} else {
-    			escritura.write("fibo<" + String.valueOf(numero) + ">s: ");
-    			if (orientacion == 'v') {
-    				escritura.newLine();
-    				escritura.write(String.valueOf(sumatoriaDeLaSerieDeFibonacci(resultado)));
-    				
-    			} else {
-    				escritura.write(String.valueOf(sumatoriaDeLaSerieDeFibonacci(resultado)));
-    			}
-    		}
-    		escritura.close();
-        } catch (Exception e) {
-        		
-        }
-    }
-    
-    private static int sumatoriaDeLaSerieDeFibonacci(final int[] numeros) {
-    	int total = 0;
-    	for (int posicion = 0; posicion < resultado.length; posicion++) {
-    		total += resultado[posicion];
-    	}
-    	return total;
-    }
+	} 
 }
--- Fibo3/src/main/java/ar/edu/untref/aydoo/SerieFibonacci.java
+++ Fibo3/src/main/java/ar/edu/untref/aydoo/SerieFibonacci.java
@@ -2,7 +2,7 @@ package ar.edu.untref.aydoo;
 
 public class SerieFibonacci {
 
-	public static int calcularElementoEnLaSerieDeFibonacci(int numero) {
+	public static int calcularElementoEnLaSerieDeFibonacci(final int numero) {
 		if (numero <= 0) {
 			return 0;
 		} else if (numero == 1) {
@@ -11,4 +11,18 @@ public class SerieFibonacci {
 			return (calcularElementoEnLaSerieDeFibonacci(numero - 1) + calcularElementoEnLaSerieDeFibonacci(numero - 2));
 		}
 	}
+	
+	public static int sumatoriaDeLaSerieDeFibonacci(final int[] numeros) {
+    	int total = 0;
+    	for (int posicion = 0; posicion < numeros.length; posicion++) {
+    		total += numeros[posicion];
+    	}
+    	return total;
+    }
+	
+	public static void calcularSerie(final int numero, final int[] resultado) {
+    	for (int numeroDeLaSerie = 0; numeroDeLaSerie < numero; numeroDeLaSerie++) {
+	    	resultado[numeroDeLaSerie] = SerieFibonacci.calcularElementoEnLaSerieDeFibonacci(numeroDeLaSerie);
+	    }
+    }
 }
